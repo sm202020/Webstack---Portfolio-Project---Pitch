3a24bdd61f999967206511d5348a6744df130321
Ojewande Taofeek <taofeekojewande@nda.edu.ng>
2024-12-08 21:18:35 +0100

    landing page created

--- stus/dashboard/src/LandingPage/landingPage.js
+++ stus/dashboard/src/LandingPage/landingPage.js
@@ -7,23 +7,50 @@ export class LandingPage extends Component {
     render() {
         return (
             <>
-                <Header />
-                <div> 
-                    <div>
-                        <h2>Manage your daily tasks</h2>
-                        <p>Easily plan and manage all your personal tasks</p>
-                    </div>
-                    <div>
-                        <p>Sign Up</p>
-                        <p>Sign In</p>
-                    </div>
+            <Header />
+            <div className={css(lpBodyStyles.lpBody)}> 
+                <div>
+                     <h2 className={css(lpBodyStyles.lpBodyHeading)}>Manage your daily tasks</h2>
+                     <p className={css(lpBodyStyles.lpBodyTag)}>Easily plan and manage all your personal tasks</p>
                 </div>
-                <Footer /> 
+                <div className={css(lpBodyStyles.lpBodyLinks)}>
+                     <button className={css(lpBodyStyles.hover)}>Sign Up</button>
+                     <button className={css(lpBodyStyles.hover)}>Sign In</button>
+                </div>
+            </div>  
+            <Footer /> 
             </>
         )
     }
 }
 
 const lpBodyStyles = StyleSheet.create({
-    
+    lpBody: {
+        position: "absolute",
+        top: "10rem",
+        left: "5rem",
+        height: "30%",
+        width: "60%",
+    },
+    lpBodyHeading: {
+        fontWeight: "bolder",
+        fontSize: "3rem",
+        marginBottom: "0",
+    },
+    lpBodyTag: {
+        marginTop: "0rem",
+        fontWeight: "bold",
+        fontSize: "1.5rem",
+    },
+    lpBodyLinks: {
+        width: "20%",
+        display: "flex",
+        justifyContent: "space-between",
+        cursor: "pointer",
+    },
+    hover: {
+        ":hover" : {
+            backgroundColor: 'aqua',
+        }
+    }
 })
